b388384afbf55e3130db32a585b1a8f1645c5055
Matt Watson <mattwat@gmail.com>
2020-03-05 10:42:46 -0500

    remove main component margin

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -4,8 +4,5 @@ export const Main = styled.main`
     background-color: inherit;
     color: inherit;
     flex: 1;
-    margin: 0 1rem;
-    @media screen and (min-width: 767px) {
-        margin: 0;
-    }
+    margin: 0;
 `
