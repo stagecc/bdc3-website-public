954c365730b11dc21590f7c91536d78af4b6dfa2
Matt Watson <mattwat@gmail.com>
2020-09-04 10:03:28 -0400

    update study details link

--- src/pages/resources/data.js
+++ src/pages/resources/data.js
@@ -50,7 +50,7 @@ const DataPage = ({ data, location }) => {
                 
                 <Paragraph center>
                     <Button light as="a" target="_blank" rel="noopener noreferrer"
-                        href="https://drive.google.com/file/d/1936teBZlvBKbQf1hmdx5JImAxJFbVoIx/view?usp=sharing"
+                        href="https://drive.google.com/file/d/10fJJ2VKFNUhk_9DHBJkWC8gwa_G9j81M/view?usp=sharing"
                         style={{ margin: '1rem', display: 'inline-flex', alignItems: 'center' }}
                     >
                         Study Details &nbsp;&nbsp; <DownloadIcon fill="var(--color-crimson)" size={ 24 } />
