477e169c3d180e4354b6790a05d682660bb3238e
Matt Watson <mattwat@gmail.com>
2020-11-11 17:32:36 -0500

    add filtering

--- src/components/data-table/data-table.js
+++ src/components/data-table/data-table.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { Fragment, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import ReactDataTable from 'react-data-table-component'
 import { Card, CardHeader, CardBody, CardFooter } from '../card'
 
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  <Fragment>
+    <input id="search" type="text" placeholder="Filter by Study Name" aria-label="Search Input" value={ filterText } onChange={ onFilter } />
+    <button type="button" onClick={ onClear }>X</button>
+  </Fragment>
+)
+
 export const DataTable = props => {
+  const [query, setQuery] = useState('')
+  const [filteredData, setFilteredData] = useState(props.data)
+
+  useEffect(() => {
+    setFilteredData(props.data.filter(item => item.Study_Name__dbGaP_Link_ && item.Study_Name__dbGaP_Link_.toLowerCase().includes(query.toLowerCase())))
+  }, [query])
+
+  const memoizedSubHeaderComponent = useMemo(() => {
+    const handleClearQuery = () => setQuery('')
+    return <FilterComponent onFilter={ e => setQuery(e.target.value)} onClear={ handleClearQuery } filterText={ query } />
+  }, [query])
+
   return (
     <Card>
       <CardHeader>Studies</CardHeader>
-      <ReactDataTable { ...props } />
+      <ReactDataTable
+        { ...props }
+        data={ filteredData }
+        subHeader
+        subHeaderComponent={ memoizedSubHeaderComponent }
+      />
       <CardFooter></CardFooter>
     </Card>
   )
--- src/components/data-table/expansion-panel.js
+++ src/components/data-table/expansion-panel.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Subheading } from '../typography'
 
 const Wrapper = styled.div`
   padding: 1rem;
@@ -8,12 +9,18 @@ const Wrapper = styled.div`
 
 export const ExpansionPanel = ({ columns, data }) => (
   <Wrapper>
-    {
-      columns.map(column => (
-        <div>
-          <strong>{ column.name }</strong>: { data[column.selector] }
-        </div>
-      ))
-    }
+    <Subheading>
+      { data.Study_Name__dbGaP_Link_ } ({ data.Study_Accession })
+    </Subheading>
+    <hr />
+    <strong>Study Description:</strong> { data.Study_Description__Verbose_ }<br />
+    <strong>Primary Research Focus:</strong> { data.Primary_Research_Focus }<br />
+    <strong>Total Number of Variables:</strong> { data.Total_Number_of_Variables }<br />
+    <strong>dbGaP Listed Variable:</strong> { data.dbGaP_Listed_Variable }<br />
+    <strong>Study Type:</strong> { data.Study_Type }<br />
+    <strong>Study-Reported Population(s):</strong> { data.Study_Reported_Population_s_ }<br />
+    <strong>Study Consent:</strong> { data.Study_Consent }<br />
+    <strong>Type of Molecular Data Available:</strong> { data.Type_of_Molecular_Data_Available }<br />
+    <strong>Primary Data Dictionary Link:</strong> { data.Primary_Data_Dictionary_Link }<br />
   </Wrapper>
 )
