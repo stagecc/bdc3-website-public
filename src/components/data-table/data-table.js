f3a7af5bc26e4ad0e15126efe2019ce810eebeb4
Matt Watson <mattwat@gmail.com>
2020-11-11 18:26:02 -0500

    add fullscreen functionality

--- src/components/data-table/data-table.js
+++ src/components/data-table/data-table.js
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import ReactDataTable from 'react-data-table-component'
 import { Card, CardHeader, CardBody, CardFooter } from '../card'
+import { CloseIcon, FullscreenIcon } from '../icons'
+import { IconButton } from '../buttons'
 
 const FilterComponent = ({ filterText, onFilter, onClear }) => (
   <Fragment>
@@ -11,12 +13,28 @@ const FilterComponent = ({ filterText, onFilter, onClear }) => (
 )
 
 export const DataTable = props => {
+  const [fullScreen, setFullScreen] = useState(false)
   const [query, setQuery] = useState('')
   const [filteredData, setFilteredData] = useState(props.data)
+
+  const fullScreenStyles = {
+    position: 'fixed',
+    left: '0',
+    top: '0',
+    width: '100vw',
+    height: '100vh',
+    zIndex: '99',
+  }
+
+  const notFullScreenStyles = {
+
+  }
 
   useEffect(() => {
     setFilteredData(props.data.filter(item => item.Study_Name__dbGaP_Link_ && item.Study_Name__dbGaP_Link_.toLowerCase().includes(query.toLowerCase())))
   }, [query])
+
+  const toggleFullScreen = () => setFullScreen(!fullScreen)
 
   const memoizedSubHeaderComponent = useMemo(() => {
     const handleClearQuery = () => setQuery('')
@@ -24,8 +42,12 @@ export const DataTable = props => {
   }, [query])
 
   return (
-    <Card>
-      <CardHeader>Studies</CardHeader>
+    <Card style={ fullScreen ? fullScreenStyles : null }>
+      <CardHeader style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <IconButton onClick={ toggleFullScreen }>
+          { fullScreen ? <CloseIcon size={ 36 } fill="#eee" /> : <FullscreenIcon size={ 36 } fill="#eee" /> }
+        </IconButton>
+      </CardHeader>
       <ReactDataTable
         { ...props }
         data={ filteredData }
--- src/components/data-table/expansion-panel.js
+++ src/components/data-table/expansion-panel.js
@@ -5,6 +5,7 @@ import { Subheading } from '../typography'
 const Wrapper = styled.div`
   padding: 1rem;
   background-color: #eee;
+  border-left: 0.5rem solid var(--color-lightgrey);
 `
 
 export const ExpansionPanel = ({ columns, data }) => (
@@ -13,14 +14,14 @@ export const ExpansionPanel = ({ columns, data }) => (
       { data.Study_Name__dbGaP_Link_ } ({ data.Study_Accession })
     </Subheading>
     <hr />
-    <strong>Study Description:</strong> { data.Study_Description__Verbose_ }<br />
-    <strong>Primary Research Focus:</strong> { data.Primary_Research_Focus }<br />
-    <strong>Total Number of Variables:</strong> { data.Total_Number_of_Variables }<br />
-    <strong>dbGaP Listed Variable:</strong> { data.dbGaP_Listed_Variable }<br />
-    <strong>Study Type:</strong> { data.Study_Type }<br />
-    <strong>Study-Reported Population(s):</strong> { data.Study_Reported_Population_s_ }<br />
-    <strong>Study Consent:</strong> { data.Study_Consent }<br />
-    <strong>Type of Molecular Data Available:</strong> { data.Type_of_Molecular_Data_Available }<br />
+    <strong>Study Description:</strong> { data.Study_Description__Verbose_ }<br /><br />
+    <strong>Primary Research Focus:</strong> { data.Primary_Research_Focus }<br /><br />
+    <strong>Total Number of Variables:</strong> { data.Total_Number_of_Variables }<br /><br />
+    <strong>dbGaP Listed Variable:</strong> { data.dbGaP_Listed_Variable }<br /><br />
+    <strong>Study Type:</strong> { data.Study_Type }<br /><br />
+    <strong>Study-Reported Population(s):</strong> { data.Study_Reported_Population_s_ }<br /><br />
+    <strong>Study Consent:</strong> { data.Study_Consent }<br /><br />
+    <strong>Type of Molecular Data Available:</strong> { data.Type_of_Molecular_Data_Available }<br /><br />
     <strong>Primary Data Dictionary Link:</strong> { data.Primary_Data_Dictionary_Link }<br />
   </Wrapper>
 )
