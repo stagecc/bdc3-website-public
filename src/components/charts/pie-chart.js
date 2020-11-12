4e1ae99291b1305b961ba69f2a3729f991340b2c
Matt Watson <mattwat@gmail.com>
2020-11-11 23:42:30 -0500

    add study charts

---
+++ src/components/charts/pie-chart.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ResponsivePie } from '@nivo/pie'
+
+export const PieChart = ({ data, height = 600 }) => {
+  return (
+    <div style={{ height: `${ height }px`, flex: '1 1 400px' }}>
+      <ResponsivePie
+        data={ data }
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={ 0.5 }
+        padAngle={ 0.7 }
+        cornerRadius={ 3 }
+        colors={{ scheme: 'red_blue' }}
+        borderWidth={ 1 }
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkColor={{ from: 'color' }}
+        sliceLabelsSkipAngle={ 10 }
+        sliceLabelsTextColor="#333333"
+      />
+    </div>
+  )
+}
--- src/components/data-table/data-table.js
+++ src/components/data-table/data-table.js
@@ -4,6 +4,8 @@ import ReactDataTable from 'react-data-table-component'
 import { Card, CardHeader, CardBody, CardFooter } from '../card'
 import { CloseIcon, FullscreenIcon } from '../icons'
 import { IconButton } from '../buttons'
+import { PieChart } from '../charts'
+import { Stat } from './stat'
 
 const FilterComponent = ({ filterText, onFilter, onClear }) => (
   <Fragment>
@@ -15,7 +17,12 @@ const FilterComponent = ({ filterText, onFilter, onClear }) => (
 export const DataTable = props => {
   const [fullScreen, setFullScreen] = useState(false)
   const [query, setQuery] = useState('')
-  const [filteredData, setFilteredData] = useState(props.data)
+  const [filteredStudies, setFilteredStudies] = useState(props.data)
+  const [typeCounts, setTypeCounts] = useState([])
+  const [populationCounts, setPopulationCounts] = useState([])
+  const [variablesCount, setVariablesCount] = useState(0)
+  const [focusCounts, setFocusCounts] = useState(0)
+  const [listedVariableCounts, setListedVariableCounts] = useState([])
 
   const fullScreenStyles = {
     position: 'fixed',
@@ -26,12 +33,43 @@ export const DataTable = props => {
     zIndex: '99',
   }
 
-  const notFullScreenStyles = {
+  const sumValues = key => filteredStudies.reduce((total, study) => total + parseInt(study[key]), 0)
 
-  }
 
   useEffect(() => {
-    setFilteredData(props.data.filter(item => item.Study_Name__dbGaP_Link_ && item.Study_Name__dbGaP_Link_.toLowerCase().includes(query.toLowerCase())))
+    const countValues = key => {
+      const counts = {}
+      filteredStudies.forEach(study => {
+        const value = study[key].trim()
+        if (counts.hasOwnProperty(value)) { counts[value] += 1 }
+          else { counts[value] = 1 }
+      })
+      return Object.keys(counts).map(key => ({ id: key, value: counts[key] }))
+    }
+    const countSemicolonDelimitedValues = key => {
+      const counts = {}
+      filteredStudies.forEach(study => {
+        const values = study[key].split(';').map(s => s.trim())
+        values.forEach(value => {
+          if (counts.hasOwnProperty(value)) { counts[value] += 1 }
+            else { counts[value] = 1 }
+        })
+      })
+      return Object.keys(counts).map(key => ({ id: key, value: counts[key] }))
+    }
+    setTypeCounts(countSemicolonDelimitedValues('Study_Type'))
+    setPopulationCounts(countSemicolonDelimitedValues('Study_Reported_Population_s_'))
+    setListedVariableCounts(countSemicolonDelimitedValues('dbGaP_Listed_Variable'))
+    setFocusCounts(countValues('Primary_Research_Focus'))
+  }, [filteredStudies])
+
+  useEffect(() => {
+    const count = filteredStudies.reduce((total, study) => total + parseInt(study.Total_Number_of_Variables), 0)
+    setVariablesCount(count)
+  }, [filteredStudies])
+
+  useEffect(() => {
+    setFilteredStudies(props.data.filter(item => item.Study_Name__dbGaP_Link_ && item.Study_Name__dbGaP_Link_.toLowerCase().includes(query.toLowerCase())))
   }, [query])
 
   const toggleFullScreen = () => setFullScreen(!fullScreen)
@@ -43,18 +81,32 @@ export const DataTable = props => {
 
   return (
     <Card style={ fullScreen ? fullScreenStyles : null }>
-      <CardHeader style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <IconButton onClick={ toggleFullScreen }>
-          { fullScreen ? <CloseIcon size={ 36 } fill="#eee" /> : <FullscreenIcon size={ 36 } fill="#eee" /> }
-        </IconButton>
-      </CardHeader>
+      <CardHeader>&nbsp;</CardHeader>
+      <CardBody>
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <Stat name="Studies" value={ filteredStudies.length } />
+          <Stat name="Variables" value={ variablesCount } />
+        </div>
+        <hr />
+        <details>
+          <summary style={{ cursor: 'pointer' }}>Charts</summary>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <PieChart data={ typeCounts } />
+            <PieChart data={ focusCounts } />
+            <PieChart data={ listedVariableCounts } />
+            <PieChart data={ populationCounts } />
+          </div>
+        </details>
+        <hr />
+      </CardBody>
+
       <ReactDataTable
         { ...props }
-        data={ filteredData }
+        data={ filteredStudies }
         subHeader
         subHeaderComponent={ memoizedSubHeaderComponent }
       />
-      <CardFooter></CardFooter>
+      <CardFooter>&nbsp;</CardFooter>
     </Card>
   )
 }
