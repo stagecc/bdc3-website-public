5e57ce5b52ebfc15104d34fc5e86ef0ae3f60d6f
Matt Watson <mattwat@gmail.com>
2020-11-12 16:29:19 -0500

    reduce gap

--- src/components/charts/pie-chart.js
+++ src/components/charts/pie-chart.js
@@ -8,8 +8,8 @@ export const PieChart = ({ data, height = 600 }) => {
         data={ data }
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
         innerRadius={ 0.5 }
-        padAngle={ 0.7 }
-        cornerRadius={ 3 }
+        padAngle={ 0.75 }
+        cornerRadius={ 1 }
         colors={{ scheme: 'red_blue' }}
         borderWidth={ 1 }
         borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
