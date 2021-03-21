573be5613d3ee9c54cebfbc6c336524edb2ef404
Matt Watson <mattwat@gmail.com>
2021-03-21 19:51:02 -0400

    autofocus search input

--- src/components/doc-search/search-form.js
+++ src/components/doc-search/search-form.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDocSearch } from './search-context'
 import { TextInput, InputGroup } from '../form'
 import { Button } from '../buttons'
 
 export const SearchForm = () => {
   const { query, handleChangeQuery, doSearch } = useDocSearch()
+  const input = useRef()
 
   const handleKeyDown = event => {
     if (event.keyCode === 13) {
@@ -12,9 +13,15 @@ export const SearchForm = () => {
     }
   }
 
+  useEffect(() => {
+    if (input.current) {
+      input.current.focus()
+    }
+  }, [input.current])
+
   return (
     <InputGroup style={{ width: '100%', maxWidth: '800px', margin: 'auto' }}>
-      <TextInput type="text" value={ query } onChange={ handleChangeQuery } onKeyDown={ handleKeyDown } />
+      <TextInput ref={ input } type="text" value={ query } onChange={ handleChangeQuery } onKeyDown={ handleKeyDown } />
       <Button onClick={ () => doSearch() }>Search</Button>
     </InputGroup>
   )
