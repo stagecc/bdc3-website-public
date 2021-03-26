9295a41d98bc69abf43b46b7de87dc4acf49fb97
Matt Watson <mattwat@gmail.com>
2021-03-26 08:55:41 -0400

    set global flag on regexp

--- src/components/doc-search/search-result.js
+++ src/components/doc-search/search-result.js
@@ -102,7 +102,7 @@ export const Result = ({ index, result }) => {
         <Content>
           <Title><ExternalLink to={ link }>{ title }</ExternalLink></Title>
           <em>{ displayLink }</em>
-          <p dangerouslySetInnerHTML={{ __html: htmlSnippet.replace('<br>', '').replace(/n?\\n/gm, '') }} />
+          <p dangerouslySetInnerHTML={{ __html: htmlSnippet.replace(/<br>/g, '').replace(/n?\\n/gm, '') }} />
         </Content>
         <Visible lg xl>
           <Thumbnail url={ thumbnailURL } />
