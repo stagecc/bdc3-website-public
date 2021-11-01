f1b39fed6e720f0697f3f05e0779acd7d7faf43c
Matt Watson <mattwat@gmail.com>
2021-11-01 13:35:23 -0400

    config ga4

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,6 +143,28 @@ module.exports = {
         cookieDomain: "biodatacatalyst.nhlbi.nih.gov", // Specifies the domain used to store the analytics cookie. Setting this to 'none' sets the cookie without specifying a domain.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-2M4JYYSBD3", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          head: false, // Puts tracking script in the head instead of the body
+          respectDNT: true, // Setting this parameter is also optional
+          exclude: [], // Avoids sending pageview hits from custom paths
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
