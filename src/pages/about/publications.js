677c980450e213c8a87c4265becd28b8d6691bc9
Matt Watson <mattwat@gmail.com>
2022-01-24 11:12:33 -0500

    [#151] remove unused vars

--- src/pages/about/publications.js
+++ src/pages/about/publications.js
@@ -1,33 +1,7 @@
 import React from "react";
 import { SEO } from "../../components/seo";
 import { PageContent } from "../../components/layout";
-import { graphql, Link } from "gatsby";
-import styled from "styled-components";
-import { Title, Heading, Meta } from "../../components/typography";
-import { InlineList2 } from "../../components/list";
-import { TagLink } from "../../components/link";
-// import { ClockIcon } from "../components/icons";
-import { useWindowWidth } from "../../hooks";
-
-const ArticlePreview = styled.article`
-  margin: 4rem 0;
-`;
-
-const Details = styled(Meta)`
-  display: flex;
-  flex-direction: ${(props) => (props.compact ? "column" : "row")};
-  justify-content: space-between;
-  & > div {
-    display: flex;
-    align-items: center;
-  }
-`;
-
-const Tags = styled(Meta)``;
-
-const PublishDate = styled.span`
-  flex: 1;
-`;
+import { Title } from "../../components/typography";
 
 const ResearchPage = ({ data }) => {
 
