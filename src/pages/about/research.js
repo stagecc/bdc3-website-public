a372f5f77667e7de090dbaae8c8162e59a1f5d1e
Sergio Rocha <rochascript@gmail.com>
2022-03-30 02:19:45 -0500

    Made edits tot the publications

--- src/pages/about/research.js
+++ src/pages/about/research.js
@@ -2,10 +2,10 @@ import React, { Fragment } from "react";
 import { SEO } from "../../components/seo";
 import { PageContent } from "../../components/layout";
 import { Heading, Paragraph, Title } from "../../components/typography";
-import { Link } from '../../components/link'
+import { Link } from "../../components/link";
 
 const ResearchPage = ({ data }) => {
-  const publications = data.publications.nodes
+  const publications = data.publications.nodes;
 
   return (
     <PageContent width="95%" maxWidth="1200px" center gutters>
@@ -16,44 +16,53 @@ const ResearchPage = ({ data }) => {
       />
       <Title>Published Research</Title>
 
-      {
-        publications.map(({ id, title, date, location, url, bdcAuthors }) => (
-          <Fragment key={id}>
-            <Heading>
-              <Link to={url}>{title}</Link>
-            </Heading>
-            <div
-              style={{
-                borderLeft: "3px solid var(--color-lightgrey)",
-                paddingLeft: "1rem",
-              }}
-            >
-              <Paragraph>
-                Published on { date } in <em>{ location }</em>
-              </Paragraph>
-              <Paragraph>
-                BioData Catalyst Authors: { bdcAuthors.map((author, i) => <span>{ author }{ i + 1 < bdcAuthors.length ? ',' : '' } </span>)}
-              </Paragraph>
-            </div>
-            <br/>
-          </Fragment>
-        ))
-    }
+      {publications.map(({ id, title, date, location, url, bdcAuthors }) => (
+        <Fragment key={id}>
+          <Heading>
+            <Link to={url}>{title}</Link>
+          </Heading>
+          <div
+            style={{
+              borderLeft: "3px solid var(--color-lightgrey)",
+              paddingLeft: "1rem",
+            }}
+          >
+            <Paragraph>
+              Published on {date} in <em>{location}</em>
+            </Paragraph>
+            <Paragraph>
+              {title ===
+              "Streamlining statistical reproducibility: NHLBI ORCHID clinical trial results reproduction"
+                ? "BioData Catalyst Consortium Authors:"
+                : "BioData Catalyst Contributor:"}{" "}
+              {bdcAuthors.map((author, i) => (
+                <span>
+                  {author}
+                  {i + 1 < bdcAuthors.length ? "," : ""}{" "}
+                </span>
+              ))}
+            </Paragraph>
+          </div>
+          <br />
+        </Fragment>
+      ))}
     </PageContent>
   );
 };
 
-export const query = graphql`query {
-  publications: allPublicationsYaml(sort: {fields: date, order: DESC}) {
-    nodes {
-      id
-      title
-      date(formatString: "MMMM Do, YYYY")
-      location
-      url
-      bdcAuthors
+export const query = graphql`
+  query {
+    publications: allPublicationsYaml(sort: { fields: date, order: DESC }) {
+      nodes {
+        id
+        title
+        date(formatString: "MMMM Do, YYYY")
+        location
+        url
+        bdcAuthors
+      }
     }
   }
-}`;
+`;
 
 export default ResearchPage;
