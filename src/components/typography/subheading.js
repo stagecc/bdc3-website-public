1b647cc570974d9928847fba0a71f9a9d07f1d62
Suejin Kim <kim.suejin.20@gmail.com>
2022-07-18 11:30:52 -0400

    add styles to subheading component

--- src/components/typography/subheading.js
+++ src/components/typography/subheading.js
@@ -6,7 +6,9 @@ import PropTypes from "prop-types";
 export const Subheading = styled.h3`
   color: ${props => (props.light ? "#dde" : "var(--color-blueberry)")};
   text-align: center;
-  font-size: 150%;
+  font-size: 130%;
+  margin-top: 0;
+  line-height: 1.5;
   @media screen and (min-width: 767px) {
     text-align: left;
     ${props => props.center && "text-align: center;"}
