8441d694c09a9c49963a9c68aa03f306cff01f1d
Suejin Kim <kim.suejin.20@gmail.com>
2022-10-12 16:40:39 -0400

    add gatsby-plugin-google-gtag package and measurement ID to gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,6 +129,12 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-2M4JYYSBD3"],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
