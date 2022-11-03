809ea16e74231ecd8002ade80261d0bb05e74a52
Suejin Kim <kim.suejin.20@gmail.com>
2022-11-03 10:21:25 -0400

    move bdc link higher up in list

--- src/pages/resources/learn.js
+++ src/pages/resources/learn.js
@@ -68,6 +68,10 @@ const resources = [
         url: "https://videocast.nih.gov/watch=37703"
       },
       {
+        text: "BioData Catalyst Videos",
+        url: "https://www.youtube.com/channel/UCGkmY5oNK8uFZzT8vV_9KgQ"
+      },
+      {
         text: "Dockstore Videos",
         url: "https://www.youtube.com/channel/UCFWNYqxQvVLAuZq8rdOSE4g/videos"
       },
@@ -86,10 +90,6 @@ const resources = [
       {
         text: "Terra Videos",
         url: "https://www.youtube.com/channel/UCkXAqpR5Hk1ZmNd2-1K2l5Q"
-      },
-      {
-        text: "BioData Catalyst Videos",
-        url: "https://www.youtube.com/channel/UCGkmY5oNK8uFZzT8vV_9KgQ"
       }
     ]
   },
