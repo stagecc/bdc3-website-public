98530576381322ad08374e141c8e669311a88ef0
Suejin Kim <kim.suejin.20@gmail.com>
2022-12-09 16:20:55 -0500

    utilize subcomponents for testimonial panel

--- src/components/carousel/panels/testimonial-panel.js
+++ src/components/carousel/panels/testimonial-panel.js
@@ -1,32 +1,19 @@
 import React from 'react'
 import styled from "styled-components";
+import { Overlay } from './subcomponents/Overlay'
+import { DetailsPanel, DetailWrapper, Headline, Description } from './subcomponents/LeftPanel'
 import { testimonialContent } from '../types'
 
-const style = {
-  margin: 0,
-  flex: 1,
-  padding: '1rem',
-  border: '10px dashed #f90',
-
-}
-
-const Overlay = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  /* flex-direction: ${props => (props.compact ? "column" : "row")}; */
-  align-items: stretch;
-`;
 
 export const TestimonialPanel = ({ content }) => {
   return (
     <Overlay>
-      <pre style={ style }>
-        { JSON.stringify(content, null, 2) }
-      </pre>
+      <DetailsPanel>
+        <DetailWrapper>
+          <Headline>{content.quote}</Headline>
+          <Description>{content.attribution}</Description>
+        </DetailWrapper>
+      </DetailsPanel>
     </Overlay>
   )
 }
