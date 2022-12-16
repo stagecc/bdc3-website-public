a5db596e3c2824273b4071067c22b13a5cb21761
Suejin Kim <kim.suejin.20@gmail.com>
2022-12-16 09:45:39 -0500

    define boundaries for data carousel content

--- src/components/carousel/types.js
+++ src/components/carousel/types.js
@@ -5,6 +5,14 @@ export const testimonialContent = PropTypes.shape({
   attribution: PropTypes.string.isRequired,
 }).isRequired
 
+// Boundaries for Testimonial Content
+// Quote:
+  // Max: 150 characters
+  // Min: 50 characters
+// Attribution:
+  // Max: 150 characters
+  // Min: none
+
 export const statsContent = PropTypes.shape({
   headline: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
@@ -16,6 +24,18 @@ export const statsContent = PropTypes.shape({
   ).isRequired,
 }).isRequired
 
+// Boundaries for Stats Content
+// Headline:
+  // Max: 75 characters
+  // Min: none
+// Description:
+  // Max: 130 characters
+  // Min: none
+// Stats:
+  // total items: no more than 2 before needing to change the style
+  // name: 35 characters
+  // value: 10 numbers without punctuation, 8-9 numbers with punctiation
+
 export const datasetInfoContent = PropTypes.shape({
   headline: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
@@ -25,6 +45,18 @@ export const datasetInfoContent = PropTypes.shape({
     }).isRequired,
   ).isRequired,
 }).isRequired
+
+// Boundaries for Stats Content
+// Headline:
+  // Max: 75 characters
+  // Min: none
+// Description:
+  // Max: 130 characters
+  // Min: none
+// Stats:
+  // total items: at least 2, no more than 4
+  // name: 70 characters (depends on length of other lines as well)
+
 
 export const panelType = PropTypes.shape({
   type: PropTypes.oneOf([
