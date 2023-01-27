ae1a9f7b40545498bb3532feee05a6f6aa95a9f5
David Glymph <Woozl@users.noreply.github.com>
2023-01-27 16:46:44 -0500

    added a diff and study count log

--- src/scripts/update-studies.js
+++ src/scripts/update-studies.js
@@ -1,5 +1,9 @@
 const fs = require('fs/promises');
+const disparity = require('disparity');
 const { queryMds, transformStudies } = require("../utils/studies-data-helpers");
+
+const STUDIES_PATH = 'src/data/studies/studies.json';
+const COVID_STUDIES_PATH = 'src/data/studies/covid-studies.json';
 
 (async function () {
   const rawMdsData = await queryMds();
@@ -7,6 +11,29 @@ const { queryMds, transformStudies } = require("../utils/studies-data-helpers");
 
   studiesStringified = JSON.stringify(studies, null, 2);
   covidStudiesStringified = JSON.stringify(covidStudies, null, 2);
-  await fs.writeFile('src/data/studies/studies.json', studiesStringified, { encoding: 'utf-8' });
-  await fs.writeFile('src/data/studies/covid-studies.json', covidStudiesStringified, { encoding: 'utf-8' });
+
+  const oldStudies = await fs.readFile(STUDIES_PATH, { encoding: 'utf-8' });
+  const oldCovidStudies = await fs.readFile(COVID_STUDIES_PATH, { encoding: 'utf-8' });
+
+  await fs.writeFile(STUDIES_PATH, studiesStringified, { encoding: 'utf-8' });
+  await fs.writeFile(COVID_STUDIES_PATH, covidStudiesStringified, { encoding: 'utf-8' });
+
+  console.log(`
+============ Studies Updated ============
+studies.json:       ${JSON.parse(oldStudies).length} -> ${studies.length}
+covid-studies.json: ${JSON.parse(oldCovidStudies).length} -> ${covidStudies.length}
+=========================================
+
+
+
+============== studies.json ============= 
+${disparity.unified(oldStudies, studiesStringified)}
+=========================================
+
+
+
+========== covid-studies.json =========== 
+${disparity.unified(oldCovidStudies, covidStudiesStringified)}
+=========================================
+`);
 })();
