2c8cb06972fb17c99cb9de4463365297e201e7f6
David Glymph <Woozl@users.noreply.github.com>
2023-01-30 16:03:28 -0500

    removed diff in script output
    this script will be used in a github action workflow, and will create a pull request which will provide a diff

--- src/scripts/update-studies.js
+++ src/scripts/update-studies.js
@@ -1,5 +1,4 @@
 const fs = require('fs/promises');
-const disparity = require('disparity');
 const { queryMds, transformStudies } = require("../utils/studies-data-helpers");
 
 const STUDIES_PATH = 'src/data/studies/studies.json';
@@ -18,28 +17,10 @@ const COVID_STUDIES_PATH = 'src/data/studies/covid-studies.json';
   await fs.writeFile(STUDIES_PATH, studiesStringified, { encoding: 'utf-8' });
   await fs.writeFile(COVID_STUDIES_PATH, covidStudiesStringified, { encoding: 'utf-8' });
 
-  const opt = { context: 0 }; // display no context lines around diff changes
-  const studiesDiff = disparity.unified(oldStudies, studiesStringified, opt);
-  const covidStudiesDiff = disparity.unified(oldCovidStudies, covidStudiesStringified, opt);
-
   console.log(`
 ============ Studies Updated ============
 studies.json:         ${JSON.parse(oldStudies).length} -> ${studies.length}
 covid-studies.json:   ${JSON.parse(oldCovidStudies).length} -> ${covidStudies.length}
 =========================================
 `);
-
-  if(studiesDiff !== '') {
-    console.log(`\n\n
-============== studies.json ============= 
-${studiesDiff}
-=========================================`);
-  }
-
-  if(covidStudiesDiff !== '') {
-    console.log(`\n\n
-========== covid-studies.json ===========
-${covidStudiesDiff}
-=========================================`);
-  }
 })();
