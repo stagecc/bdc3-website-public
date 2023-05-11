e90da0584d04761820a02d5b371f62d6aac1a6b0
suejinkim20 <kim.suejin.20@gmail.com>
2023-05-11 00:47:15 -0400

    add events to learn page

--- src/pages/resources/learn.js
+++ src/pages/resources/learn.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { graphql } from "gatsby";
 import { SEO } from "../../components/seo";
 import { PageContent } from "../../components/layout";
 import { Title, Paragraph, Heading, Subheading } from "../../components/typography";
-import { Container as Grid, Row, Col } from "react-grid-system";
+import { Container as ReactGrid, Row, Col } from "react-grid-system";
 import { ResourceCard } from "../../components/card";
 import { Link } from "../../components/link";
 import { Markdown } from "../../components/markdown"
 import { ButtonLink } from "../../components/buttons"
+import { Module } from "../../components/layout";
+import { HorizontalRule } from "../../components/horizontal-rule"
+import { Grid } from '@mui/material'
 import {
   DocumentsIcon,
   EstimateIcon,
@@ -82,8 +86,58 @@ const moreAnswers = [
   }
 ]
 
+const EventsList = ({ title, events }) => {
+  // const { isCompact } = useWindowWidth();
+  return (
+    <Module title={title}>
+      <br />
+      {events.length ? (
+        events.map((event) => {
+          const { excerpt } = event.node;
+          const {
+            title,
+            path,
+            date,
+            display_date,
+          } = event.node.frontmatter;
+          return (
+            <Fragment key={title}>
+              <Grid container spacing={2}>
+                <Grid item sm={12} md={3}>
+                  <Paragraph left noMargin>
+                    {display_date ? display_date : date}
+                  </Paragraph>
+                </Grid>
+                <Grid item sm={12} md={9}>
+                  <Subheading left>
+                    <Link 
+                      to={path}
+                    >{title}</Link>
+                  </Subheading>
+                </Grid>
+              </Grid>
+              <HorizontalRule />
+              <br/>
+            </Fragment>
+          );
+        })
+      ) : (
+        <Paragraph center>
+          There are no events to display at the moment. Please check back soon!
+        </Paragraph>
+      )}
+    </Module>
+  );
+};
 
-const LearnPage = () => (
+const LearnPage = ({data}) => {
+  const todaysDate = new Date();
+
+  const events = data.events.edges.filter(
+    ({ node }) => new Date(node.frontmatter.date) >= todaysDate
+  );
+
+  return (
   <PageContent width="95%" maxWidth="1200px" center gutters>
     <SEO title="Learn" description="" keywords="" />
 
@@ -93,7 +147,7 @@ const LearnPage = () => (
     <Subheading center>Quick Start on BDC</Subheading>
     <Paragraph center>Download one or all of our quick start guides to get started on BDC.</Paragraph>
     
-    <Grid fluid>
+    <ReactGrid fluid>
       <Row>
         {quickStart.map(resource => (
           <Col
@@ -110,15 +164,17 @@ const LearnPage = () => (
           </Col>
         ))}
       </Row>
-    </Grid>
+    </ReactGrid>
 
     <Subheading center>Training Events</Subheading>
-    <p>Display Events here</p>
-
+    <p>Next Event:</p>
+    <EventsList events={events} />
+    <ButtonLink to="/about/events/" style={{margin: 'auto'}}>See All Upcoming Events</ButtonLink>
+    
     <Heading center>Find More Answers</Heading>
     <Paragraph center>View documentation, connect with others on the community forum, browse BDC Community Hours materials, or get in touch with a BDC representative.</Paragraph>
 
-    <Grid fluid>
+    <ReactGrid fluid>
       <Row>
         {moreAnswers.map(resource => (
           <Col
@@ -135,9 +191,33 @@ const LearnPage = () => (
           </Col>
         ))}
       </Row>
-    </Grid>
+    </ReactGrid>
     
   </PageContent>
 );
-
+}
 export default LearnPage;
+
+export const query = graphql`
+  query {
+    events: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: {
+        fileAbsolutePath: { regex: "/events/" }
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            display_date
+            path
+            title
+            url
+          }
+          excerpt(pruneLength: 280)
+        }
+      }
+    }
+  }
+`;
