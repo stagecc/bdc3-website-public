8eaf509462f022077ee60d34cdbef455bd427d14
suejinkim20 <kim.suejin.20@gmail.com>
2023-06-15 15:36:12 -0400

    add speaker image component to latest updates article template

--- src/templates/article-template.js
+++ src/templates/article-template.js
@@ -1,5 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 import { graphql} from "gatsby";
+import Img from "gatsby-image";
 import { SEO } from "../components/seo";
 import { Title, Subtitle, Meta } from "../components/typography";
 import { InlineList2 } from "../components/list";
@@ -9,6 +11,22 @@ import { Visible } from "react-grid-system";
 import { HorizontalRule } from "../components/horizontal-rule";
 import { Link } from "../components/link";
 import { Markdown } from '../components/markdown'
+
+const SpeakerImageWrapper = styled.div`
+  @media screen and (max-width: 650px){
+    width: 100%
+  }
+`
+const SpeakerImage = styled(Img)`
+  margin: 0 2rem 1rem 0;
+  width: 240px;
+  /* filter: drop-shadow(10px 10px 8px rgba(0, 0, 0, 0.1)); */
+  float: left;
+  @media screen and (max-width: 650px){
+    float: none;
+    margin: 0 auto 1rem;
+  }
+`
 
 export default ({ data, pageContext }) => {
   const { markdownRemark: { frontmatter, rawMarkdownBody } } = data;
@@ -36,6 +54,11 @@ export default ({ data, pageContext }) => {
               ))}
             />
           </Meta>
+          {frontmatter.speakerImage && (
+            <SpeakerImageWrapper>
+              <SpeakerImage fluid={frontmatter.speakerImage.childImageSharp.fluid}/>
+            </SpeakerImageWrapper>
+          )}
           <Markdown src={ rawMarkdownBody } />
         </div>
       </div>
@@ -86,6 +109,13 @@ export const newsItemQuery = graphql`
           title
           description
           keywords
+        }
+        speakerImage {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }  
         }
       }
     }
