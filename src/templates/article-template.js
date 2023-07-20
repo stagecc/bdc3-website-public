20dad5a97a9a58f54145331f51bb051ee3c7177f
suejinkim20 <kim.suejin.20@gmail.com>
2023-07-20 16:35:45 -0400

    WIP copy components from fellows page to create hexagonal headshot images

--- src/templates/article-template.js
+++ src/templates/article-template.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { graphql} from "gatsby";
 import Img from "gatsby-image";
 import { SEO } from "../components/seo";
-import { Title, Subtitle, Meta } from "../components/typography";
+import { Title, Subtitle, Meta, Heading } from "../components/typography";
 import { InlineList2 } from "../components/list";
 import { TagLink } from "../components/link";
 import { PageContent } from "../components/layout";
@@ -11,6 +11,7 @@ import { Visible } from "react-grid-system";
 import { HorizontalRule } from "../components/horizontal-rule";
 import { Link } from "../components/link";
 import { Markdown } from '../components/markdown'
+import { useWindowWidth } from "../hooks";
 
 const SpeakerImageWrapper = styled.div`
   @media screen and (max-width: 650px){
@@ -27,10 +28,44 @@ const SpeakerImage = styled(Img)`
     margin: 0 auto 1rem;
   }
 `
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: ${props => (props.compact ? "column" : "row")};
+  justify-content: center;
+  align-items: ${props => (props.compact ? "center" : "flex-start")};
+  margin-bottom: 3rem;
+ 
+`;
+
+const PhotoWrapper = styled.div`
+  margin: 2rem;
+  max-width: 231px;
+  max-height: 200px;
+  min-width: 231px;
+  min-height: 200px;
+  clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+  filter: drop-shadow(0 0 0.25rem rgba(0, 0, 0, 0.2));
+`;
+
+const FellowPhoto = styled(Img)`
+  max-width: 231px;
+  max-height: 200px;
+  min-width: 231px;
+  min-height: 200px;
+  clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+  transition: filter 250ms, transform 500ms;
+  transform: scale(1);
+  transform-origin: center center;
+`;
+
+const FellowDetails = styled.div`
+  flex: 1;
+`;
 
 export default ({ data, pageContext }) => {
   const { markdownRemark: { frontmatter, rawMarkdownBody } } = data;
   const { prev, next } = pageContext;
+  const { isCompact } = useWindowWidth();
 
   return (
     <PageContent width="95%" maxWidth="1200px" center gutters>
@@ -63,14 +98,23 @@ export default ({ data, pageContext }) => {
           <HorizontalRule />
 
           {/* author block */}
+          <Heading>About the Authors</Heading>
+
           {frontmatter.authors && frontmatter.authors.map((author)=> (
             <div>
-              {author.name && (
-                <h3>{author.name}</h3>
-              )}
-              {author.description && (
-                <Markdown src={author.description}/>
-              )}
+                <FlexWrapper compact={isCompact}>
+                  <PhotoWrapper>
+                    <FellowPhoto fixed={frontmatter.speakerImage.childImageSharp.fluid} />
+                  </PhotoWrapper>
+                  <FellowDetails>
+                    {author.name && (
+                      <h3>{author.name}</h3>
+                    )}
+                    {author.description && (
+                      <Markdown src={author.description}/>
+                    )}
+                  </FellowDetails>
+                </FlexWrapper>
             </div>
           ))}
 
