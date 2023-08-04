c8b896bc017373123e704b2a2863bd08d852937d
Matt Watson <mwatson@renci.org>
2023-08-04 14:27:06 -0400

    set up result inspection dialog w mui
    improve dialog style
    
    render results header only when results are rendered
    
    refactor; go all-in on mui

--- src/components/search/results.js
+++ src/components/search/results.js
@@ -1,79 +1,105 @@
-import React, { Fragment, useCallback } from "react";
+import React, { Fragment, useCallback, useState } from "react";
 import { useSearch } from './context'
-import { Pagination } from '@mui/material'
+import {
+  Box,
+  Grid,
+  Pagination,
+  Stack,
+} from '@mui/material'
 import { Dots } from '../loading'
+import { ResultDetails } from './result-details'
 
 //
 
+const ResultCard = ({ result, onClick }) => {
+  return (
+    <Box
+      as="pre"
+      sx={{
+        backgroundColor: '#eee',
+        whiteSpace: 'pre-wrap',
+        fontSize: '75%',
+        overflow: 'hidden',
+        maxHeight: '300px',
+        border: '1px solid var(--color-blueberry)',
+        cursor: 'pointer',
+        padding: '0.5rem',
+        filter: 'opacity(0.5)',
+        transition: 'filter 250ms',
+        '&:hover': {
+          filter: 'opacity(1.0)',
+        }
+      }}
+    >{ JSON.stringify(result, null, 1) }</Box>
+  )
+}
+
 export const Results = () => {
   const {
-    currentPage, doSearch, isLoading, pageCount, query, resultCount, results,
+    currentPage, doSearch, isLoading, pageCount, query, PER_PAGE,
+    resultCount, results, setSelectedResult,
   } = useSearch()
-  
+
   const ResultsHeader = useCallback(() => {
+    const resultSpan = `${ (currentPage - 1) * PER_PAGE + 1 } to ${ Math.min(currentPage * PER_PAGE, resultCount) }`
     return (
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          { resultCount } concepts found for &ldquo;{ query }&rdquo;
-        </div>
-        <div>
-          { pageCount } pages
-        </div>
-        <div>
-          <Pagination
-            defaultPage={1}
-            page={currentPage}
-            count={pageCount}
-            onChange={(event, page) => doSearch(query, page)}
-          />
-        </div>
-      </div>
+      <Stack
+        direction={{ xs: 'column', md: 'row' }}
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          gap: '2rem',
+        }}
+      >
+        <Box>Showing { resultSpan } of { resultCount } concepts found for &ldquo;{ query }&rdquo;</Box>
+        <Pagination
+          defaultPage={ 1 }
+          page={ currentPage }
+          count={ pageCount }
+          onChange={ (event, page) => doSearch(query, page) }
+        />
+      </Stack>
     )
-  }, [currentPage, doSearch, pageCount, query, resultCount])
+  }, [currentPage, doSearch, pageCount, PER_PAGE, query, resultCount])
 
   return (
     <Fragment>
       <br />
   
-      <ResultsHeader />
-
       {
         isLoading ? (
-          <div style={{
+          <Box sx={{
             minHeight: '400px',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-          }}><Dots textPlacement="top" /></div>
+          }}><Dots textPlacement="top" /></Box>
         ) : (
           <Fragment>
-            <div style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr)',
-              gridGap: '1rem',
-            }}>
+            <ResultsHeader />
+
+            <Grid container spacing={{ xs: 2, md: 3 }}>
               {
                 results.map((result, i) => (
-                  <pre
-                    key={ `${i}_${result.id}` }
-                    style={{
-                      whiteSpace: 'pre-wrap',
-                      fontSize: '75%',
-                      overflow: 'auto',
-                      maxHeight: '300px',
-                      border: '2px solid crimson',
-                    }}
-                  >{ JSON.stringify(result, null, 2) }</pre>
+                  <Grid item key={ `${i}_${result.id}` }
+                    xs={ 12 } md={ 6 }
+                    onClick={ () => setSelectedResult(result) }
+                  >
+                    <ResultCard result={ result } />
+                  </Grid>
                 ))
-            }
-            </div>
-            
+              }
+            </Grid>
+
             <br />
 
             <ResultsHeader />
           </Fragment>
         )
       }
+
+      <ResultDetails />
       
     </Fragment>
   )
