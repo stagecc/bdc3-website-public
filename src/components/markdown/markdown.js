83918b41cafda7dabf818bdc06a28cc88a221467
suejinkim20 <kim.suejin.20@gmail.com>
2023-08-22 17:09:14 -0400

    remove img from Markdown component map

--- src/components/markdown/markdown.js
+++ src/components/markdown/markdown.js
@@ -7,8 +7,6 @@ export const Markdown = ({ src }) => {
     a: function Anchor({ node, href, children, ...props }) { return <Link to={ href } { ...props }>{ children }</Link> },
     p: function Anchor({ node, href, children, ...props }) { return <p style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</p> },
     li: function Anchor({ node, href, children, ...props }) { return <li style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</li> },
-    img: function Anchor({ node, href, children, ...props }) { return <img  { ...props } /> }
-
   }), [])
 
   return (
