ec1f1a8fa267952255c5964939421b5a61e16b9c
Matt Watson <mwatson@renci.org>
2023-08-25 10:48:39 -0400

    create mui search form
    Squashed commit of the following:
    
    commit 474e893df8f3746a0c2ed7bbb38bac54777edb0f
    Author: Matt Watson <mwatson@renci.org>
    Date:   Fri Aug 25 10:47:57 2023 -0400
    
        style, add clear button
    
    commit 89d9a6a6898c89d025dc6e852c8316a36007b85a
    Author: Matt Watson <mwatson@renci.org>
    Date:   Wed Aug 23 12:43:44 2023 -0400
    
        wip: new form
    
    wire up slash key focus option to search form text field
    
    add focus styling to input
    
    focus input field when query is cleared

--- src/components/search/form.js
+++ src/components/search/form.js
@@ -6,6 +6,164 @@ import { Button } from '../buttons'
 import { MagnifyingGlassIcon } from '../icons'
 import { useSearch } from './context'
 
+import {
+  Divider,
+  Fade,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  Paper,
+  Tooltip,
+} from '@mui/material'
+import {
+  Directions as DirectionsIcon,
+  Tune as FiltersIcon,
+  Search as SearchIcon,
+  Backspace as ClearQueryIcon,
+} from '@mui/icons-material'
+
+//
+
+export const DugForm = ({ focusOnMount = false, slashFocus = false }) => {
+  const { doSearch, query } = useSearch()
+  const [formQuery, setFormQuery] = useState(query)
+  const inputRef = useRef()
+  
+  const handleChangeFormQuery = event => {
+    setFormQuery(event.target.value)
+  }
+
+  const handleClickClearQuery = event => {
+    setFormQuery('')
+    if (!inputRef.current) {
+      return
+    }
+    inputRef.current.focus()
+  }
+
+  const handleClickSubmit = event => {
+    event.preventDefault()
+    doSearch(formQuery)
+  }
+
+  useEffect(() => {
+    if (focusOnMount && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [focusOnMount])
+
+  useEffect(() => {
+    if (!slashFocus) {
+      return
+    }
+    // this lets the user jump focus to the search box
+    // with the forward slash "/" key
+    const handleKeyPress = event => {
+      if (inputRef.current) {
+        const inputFocus = inputRef.current.input === document.activeElement
+        if (!inputFocus) {
+          if (event.key === '/') {
+            event.preventDefault()
+            inputRef.current.focus()
+            inputRef.current.select()
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+          } else {
+            // Keypress with no associated function has been fired on the page.
+            // message.open({
+            //   content: `use "/" to focus the search box.`
+            // })
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeyPress)
+    return () => window.removeEventListener('keydown', handleKeyPress)
+  }, [])
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={ handleClickSubmit }
+      sx={{
+        p: '4px 8px',
+        display: 'flex',
+        alignItems: 'center',
+        border: '1px solid transparent',
+        backgroundColor: '#eceef3',
+        transition: 'background-color 250ms',
+        '&:focus-within': {
+          backgroundColor: '#eef3f9',
+          border: '1px solid #dee3e9',
+        },
+        borderRadius: '8px',
+        '.MuiIconButton-root': { p: 1 },
+        '& .clear-button': {
+          transition: 'filter 250ms',
+          filter: 'opacity(0.25) saturate(0.0)',
+        },
+        '&:hover .clear-button': { filter: 'opacity(0.5) saturate(0.25)' },
+        '&:hover .clear-button:hover': { filter: 'opacity(1.0) saturate(1.0)' },
+        '.slash-key': {
+          position: 'relative',
+          backgroundColor: '#ddd9',
+          borderTop: '3px solid #ddd6',
+          borderRight: '3px solid #ccc9',
+          borderBottom: '3px solid #ccc9',
+          borderLeft: '3px solid #ddd6',
+          borderRadius: '2px',
+          width: '22px', height: '22px',
+          ml: 1,
+          pointerEvents: 'none',
+          '&::after': {
+            position: 'absolute',
+            left: 0, right: 0,
+            top: 0, bottom: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            content: '"/"',
+            color: '#6669',
+            pt: 0.5,
+          },
+        },
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1, p: 1 }}
+        placeholder="Search for biomedical concepts"
+        inputProps={{ 'aria-label': 'search google maps' }}
+        value={ formQuery }
+        onChange={ handleChangeFormQuery }
+        inputRef={ inputRef }
+        endAdornment={
+          <Fragment>
+            <Fade in={ !!formQuery }>
+              <InputAdornment position="end">
+                <Tooltip title="Clear query" placement="left">
+                  <IconButton
+                    aria-label="clear query"
+                    color="primary"
+                    size="small"
+                    className="clear-button"
+                    onClick={ handleClickClearQuery }
+                  >
+                    <ClearQueryIcon fontSize="small" />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            </Fade>
+            { slashFocus && <span className="slash-key" /> }
+          </Fragment>
+        }
+      />
+
+      <IconButton type="submit" aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
+
 const SearchButton = styled(Button)`
   background-color: var(--color-blueberry);
   border-top-left-radius: 0;
