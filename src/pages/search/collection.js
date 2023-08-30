435e293f9245db155d23bdc2006cf654d7a7848e
Matt Watson <mwatson@renci.org>
2023-08-30 15:30:33 -0400

    scaffold two-step checkout flow
    Squashed commit of the following:
    
    commit c091585ff955592635b72bccf3ff9021dbec25bf
    Author: Matt Watson <mwatson@renci.org>
    Date:   Wed Aug 30 14:18:33 2023 -0400
    
        disable cart-related buttons when cart empty
    
    commit 8f1efccc3d8b227d4dc01c563212ca2756670119
    Author: Matt Watson <mwatson@renci.org>
    Date:   Wed Aug 30 13:30:31 2023 -0400
    
        style in/complete steps accordingly
    
    commit 32a10b99251063e35ffe43111be2ebadd7cc808a
    Author: Matt Watson <mwatson@renci.org>
    Date:   Tue Aug 29 14:28:31 2023 -0400
    
        implement multi-step checkout flow
    
        combine first and second checkout steps
    
    commit 7bd3d465ffe01b01f216009f7e601225a3a6d1c5
    Author: Matt Watson <mwatson@renci.org>
    Date:   Sat Aug 26 10:50:38 2023 -0400
    
        detail cart contents for first next step
    
        wip
    
        wip
    
        update next steps directives
    
        outline cart contents

--- src/pages/search/collection.js
+++ src/pages/search/collection.js
@@ -1,19 +1,83 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { navigate } from 'gatsby'
 import {
-  Button, Card, CardActionArea, CardContent, CardHeader,
-  Divider, IconButton, Stack, Tooltip, Typography,
+  Box, Button, Card, CardActionArea, CardActions, CardContent, CardHeader,
+  Divider, Grid, Stack, Step, Stepper, StepLabel, Typography, useTheme,
 } from '@mui/material'
+import {
+  KeyboardArrowLeft as BackIcon,
+  Download as DownloadIcon,
+  KeyboardArrowRight as ForwardIcon,
+} from '@mui/icons-material'
 import { PageContent } from '../../components/layout'
 import { SEO } from '../../components/seo'
-import { Link } from '../../components/link'
 import { useSearch } from '../../components/search'
-import { DeleteIcon as ClearCollectionIcon } from '../../components/icons'
 import { downloadJson } from '../../utils'
+import { Link } from '../../components/link'
 
 //
 
-const CollectionPage = () => {
+const steps = [
+  {
+    title: 'Check Access',
+    content: 'Log in to dbGaP with ERA Commons to determine access account voluptate minim dolore voluptate est qui adipisicing velit mollit et deserunt commodo ad laboris non consectetur ad laboris non consectetur.',
+    color: '#efece3',
+  },
+  {
+    title: 'Build a Cohort',
+    content: 'By building a cohort, Lorem ipsum magna aliqua adipisicing dolore ullamco est magna reprehenderit laborum anim est nisi elit.',
+    color: '#ece3ef',
+  },
+  {
+    title: 'Begin Analysis',
+    content: 'Utilize Seven Bridges to start your analysis, laboris ad magna sunt in adipisicing deserunt occaecat nulla magna dolore laboris irure esse ex adipisicing sunt laborum amet ex adipisicing sunt laborum amet.',
+    color: '#e3efec',
+  },
+  {
+    title: 'Continue Search',
+    content: 'Find more data with PIC-SURE or Gen3, eu minim sint proident magna cupidatat sit dolore exercitation veniam pariatur minim sunt est culpa.',
+    color: '#e3ecef',
+  },
+]
+
+//
+
+const StepIndicator = ({ activeIndex }) => {
+  const theme = useTheme()
+
+  return (
+    <Stepper
+      activeStep={ activeIndex }
+      sx={{
+        m: 4,
+        '.Mui-completed': {
+          fontWeight: 'bold',
+          color: theme.palette.secondary.dark,
+        },
+        '.Mui-active': {
+          color: theme.palette.primary.main,
+        },
+        '.Mui-disabled': {
+        },
+      }}
+    >
+      <Step>
+        <StepLabel>Confirm Selections</StepLabel>
+      </Step>
+      <Step>
+        <StepLabel>Next Steps</StepLabel>
+      </Step>
+    </Stepper>
+  )
+}
+
+//
+
+const ReviewStep = () => {
+  const{ goToPreviousStep, goToNextStep } = useCheckout()
   const { cart } = useSearch()
+
+  const { concepts, studies, variables } = cart.contents
 
   const handleClickDownloadAsJson = event => {
     event.preventDefault()
@@ -22,90 +86,238 @@ const CollectionPage = () => {
   }
 
   return (
-    <PageContent width="95%" maxWidth="1400px" center gutters>
-      <SEO
-        title="Favorites - Semantic Search"
-        description="Favorites - BioData Catalyst semantic search provided by Dug"
-      />
+    <Card sx={{
+      height: '100%',
+      '.MuiButton-root.MuiButton-sizeLarge': {
+        minHeight: '75px',
+      },
+      '.download-button': {
+        borderRadius: '18px',
+      },
+      '.MuiCardActions-root': {
+        display: 'flex',
+        justifyContent: 'space-between',
+      },
+      '& summary': {
+        cursor: 'pointer',
+        p: 1,
+      },
+      '.empty': {
+        backgroundColor: '#eee',
+        filter: 'opacity(0.75)',
+      },
+      '.MuiTypography-root.details': {
+        p: 1,
+        color: '#666',
+      },
+    }}>
+      <CardHeader title="Review Selections" />
 
-      <section>
-        <Typography variant="h1">Semantic Search: My Collection</Typography>
+      <Divider />
 
-        <Card>
-          <CardHeader title={ `Concepts (${ cart.contents.concepts.length })` } />
-          <Divider />
-          <CardContent component="pre" sx={{ backgroundColor: '#eee', fontSize: '75%', m: 0 }}>
-            { JSON.stringify(cart.contents.concepts, null, 2) }
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader title={ `Studies (${ cart.contents.studies.length })` } />
-          <Divider />
-          <CardContent component="pre" sx={{ backgroundColor: '#eee', fontSize: '75%', m: 0 }}>
-            { JSON.stringify(cart.contents.studies, null, 2) }
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader title={ `Variables (${ cart.contents.variables.length })` } />
-          <Divider />
-          <CardContent component="pre" sx={{ backgroundColor: '#eee', fontSize: '75%', m: 0 }}>
-            { JSON.stringify(cart.contents.variables, null, 2) }
-          </CardContent>
-        </Card>
+      <CardContent className={ !concepts.length ? 'empty' : '' }>
+        <Typography variant="h6">Concepts</Typography>
+        {
+          concepts.length
+            ? concepts.map(({ id, name, type, description }, i) => (
+              <details key={ `cart-concepts-${ id }` }>
+                <Typography component="summary">{ i + 1 }. { name }</Typography>
+                <Typography className="details">
+                  • id: { id } <br />
+                  • type: { type } <br />
+                  • description: { description }
+                </Typography>
+              </details>
+            ))
+          : <Typography paragraph className="details">None selected.</Typography>
+        }
+      </CardContent>
       
-        <Stack direction={{ xs: 'column', md: 'row' }} justifyContent="space-around">
-          <Link to="/search">Return to search</Link>
+      <Divider />
 
-          <Button onClick={ handleClickDownloadAsJson }>Download as JSON</Button>
+      <CardContent className={ !studies.length ? 'empty' : '' }>
+        <Typography variant="h6">Studies</Typography>
+        {
+          studies.length
+            ? studies.map(({ id, name, url, source }, i) => (
+              <details key={ `cart-concepts-${ id }` }>
+                <Typography component="summary">{ i + 1 }. { name }</Typography>
+                <Typography className="details">
+                  • source: { source } <br />
+                  • id/url: <Link to={ url }>{ id }</Link>
+                </Typography>
+              </details>
+            ))
+            : <Typography paragraph className="details">None selected.</Typography>
+        }
+      </CardContent>
+      
+      <Divider />
 
-          <Tooltip title="Clear Collection" placement="left">
-            <IconButton onClick={ () => cart.clear() } sx={{ filter: 'opacity(0.5)', transition: 'filter 250ms', '&:hover': { filter: 'opacity(1.0)' } }}>
-              <ClearCollectionIcon size={ 24 } fill="var(--color-crimson)" />
-            </IconButton>
-          </Tooltip>
-        </Stack>
-      </section>
+      <CardContent className={ !variables.length ? 'empty' : '' }>
+        <Typography variant="h6">Variables</Typography>
+        {
+          variables.length
+            ? variables.map(({ id, name, description, url }, i) => (
+              <details key={ `cart-concepts-${ id }` }>
+                <Typography component="summary">{ i + 1 }. { name }</Typography>
+                <Typography className="details">
+                  • description: { description } <br />
+                  • id/url: <Link to={ url }>{ id }</Link>
+                </Typography>
+              </details>
+            ))
+            : <Typography className="details">None selected.</Typography>
+        }
+      </CardContent>
 
-      <Divider sx={{ my: 8 }} />
+      <Divider />
 
-      <section>
-        <Typography variant="h2">Next Steps</Typography>
+      <CardContent>
+          <br /><br />
+        <Typography paragraph align="center">
+          Several of the services available in the next steps can make use of your concept, study, and variable selections here.
+          Use the button below to download your selections as JSON. Then proceed to the next step.
+          <br /><br />
+          <Button
+            variant="contained"
+            color="info"
+            startIcon={ <DownloadIcon /> }
+            onClick={ handleClickDownloadAsJson }
+            className="download-button"
+            disabled={ cart.count === 0 }
+          >Download Selections</Button>
+          <br /><br />
+        </Typography>
+      </CardContent>
 
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          gap={ 4 }
-          sx={{ width: '100%', '.MuiCard-root': { flex: 1 } }}
-        >
-          <Card>
-            <CardActionArea>
-              <CardHeader title="Check Access" />
-              <CardContent>
-                Voluptate minim dolore voluptate est qui adipisicing velit mollit et deserunt commodo ad laboris non consectetur.
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card>
-            <CardActionArea>
-              <CardHeader title="Analyze" />
-              <CardContent>
-                Laboris ad magna sunt in adipisicing deserunt occaecat nulla magna dolore laboris irure esse ex adipisicing sunt laborum amet.
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card>
-            <CardActionArea>
-              <CardHeader title="Continue search" />
-              <CardContent>
-                With Gen3 or PIC-SURE, eu minim sint proident magna cupidatat sit dolore exercitation veniam pariatur minim sunt est culpa.
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Stack>
-      </section>
+      <Divider />
+      
+      <CardActions>
+        <Button
+          variant="outlined"
+          size="large"
+          startIcon={ <BackIcon /> }
+          onClick={ () => navigate(-1) }
+        >Return to Search</Button>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          endIcon={ <ForwardIcon /> }
+          onClick={ goToNextStep }
+          disabled={ cart.count === 0 }
+        >Next Steps</Button>
+      </CardActions>
+    </Card>
+  )
+}
 
-    </PageContent>
+//
+
+const NextStepCard = ({ title, content, color = '#eee', onClick }) => {
+  return (
+    <Card className="next-step-card" sx={{
+      height: '100%',
+      backgroundColor: color,
+      '.MuiCardHeader-root': {
+        minHeight: '150px',
+      },
+      '.MuiCardHeader-title': {
+        fontSize: '2rem',
+        textAlign: 'center',
+      },
+      '.MuiCardActionArea-root': {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+      },
+    }}>
+      <CardActionArea onClick={ onClick }>
+        <CardHeader title={ title } />
+        <Divider />
+        <CardContent sx={{ height: '100%' }}>
+          { content }
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+const NextSteps = () => {
+  const{ goToPreviousStep } = useCheckout()
+
+  return (
+    <Box>
+      <Typography variant="h4">Next Steps</Typography>
+      <Grid container spacing={ 4 }>
+        {
+          steps.map(({ title, content, color }) => (
+            <Grid item
+              key={ `step-${ title }` }
+              xs={ 12 } md={ 6 }
+            >
+              <NextStepCard
+                title={ title }
+                content={ content }
+                color={ color }
+                onClick={ console.log }
+              />
+            </Grid>
+          ))
+        }
+        <Grid item sx={ 12 }>
+          <Button
+            variant="outlined"
+            size="large"
+            startIcon={ <BackIcon /> }
+            onClick={ () => goToPreviousStep() }
+            sx={{ backgroundColor: '#fff' }}
+          >Back to Collection</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+//
+
+const STEPS = [
+  ReviewStep,
+  NextSteps,
+]
+
+const CheckoutContext = React.createContext({ })
+const useCheckout = () => React.useContext(CheckoutContext)
+
+const CollectionPage = () => {
+  const [currentStep, setCurrentStep] = useState(0)
+
+  const goToNextStep = () => setCurrentStep((currentStep + 1) % 2)
+  const goToPreviousStep = () => setCurrentStep((currentStep + 1) % 2)
+
+  return (
+    <CheckoutContext.Provider value={{ goToPreviousStep, goToNextStep }}>
+      <PageContent width="95%" maxWidth="1400px" center gutters>
+        <SEO
+          title="Collection - Semantic Search"
+          description="Collection - BioData Catalyst semantic search provided by Dug"
+        />
+
+        <Typography hidden variant="h1">Semantic Search: Collection</Typography>
+
+        <Box sx={{
+          maxWidth: '800px',
+          margin: 'auto',
+        }}>
+          <StepIndicator activeIndex={ currentStep } />
+
+          { STEPS.map((Step, i) => currentStep === i && <Step />) }
+        </Box>
+
+      </PageContent>
+    </CheckoutContext.Provider>
   );
 }
 
