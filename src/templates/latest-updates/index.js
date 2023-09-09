2a0def14e540dda76a1c9b34d3dc99509ea99383
Matt Watson <mwatson@renci.org>
2023-09-09 10:46:19 -0400

    cleanup
    extract author card to separate component
    
    add map keys
    
    remove superfluous divs
    
    extract tags list to separate component
    
    minor cleanup
    
    remove unused vars

--- src/templates/latest-updates/index.js
+++ src/templates/latest-updates/index.js
@@ -14,17 +14,16 @@ import { Markdown } from "../../components/markdown"
 import { useWindowWidth } from "../../hooks";
 import './module.css'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import ButtonBase from '@mui/material/ButtonBase'
+import {
+  Button,
+  Collapse,
+  Stack,
+} from '@mui/material'
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({  expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-}));
+const ExpandIcon = ({ expanded }) => <ExpandMoreIcon sx={{
+  transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)',
+  transition: 'transform 250ms',
+}} />;
 
 const SpeakerImageWrapper = styled.div`
   @media screen and (max-width: 650px){
@@ -40,25 +39,21 @@ const SpeakerImage = styled(Img)`
     margin: 0 auto 1rem;
   }
 `
-const FlexWrapper = styled.div`
+const FlexWrapper = styled.div(({ compact }) => (`
   display: flex;
-  flex-direction: ${props => (props.compact ? "column" : "row")};
+  gap: 1rem;
+  flex-direction: ${compact ? 'column' : 'row'};
   justify-content: center;
-  align-items: ${props => (props.compact ? "center" : "flex-start")};
+  align-items: ${compact ? 'center' : 'flex-start'};
   margin-bottom: 2rem;
   background-color: #EFEFEF;
-  padding: 0.75rem 2rem 0.5rem;
+  padding: 1rem;
   border-radius: 5px;
   filter: drop-shadow(5px 5px 8px rgba(0, 0, 0, 0.1));
-`;
+`));
 
 const PhotoWrapper = styled.div`
-  margin: 1.75rem 1rem;
-  max-width: 231px;
-  max-height: 200px;
-  min-width: 231px;
-  min-height: 200px;
-  /* clip-path: circle(60%); */
+  margin: 1rem;
   filter: drop-shadow(0 0 0.25rem rgba(0, 0, 0, 0.2));
 `;
 
@@ -68,21 +63,20 @@ const AuthorPhoto = styled(Img)`
   min-width: 200px;
   min-height: 200px;
   clip-path: circle(100%);
-  transition: filter 250ms, transform 500ms;
-  transform: scale(1);
-  transform-origin: center center;
+  transition: filter 250ms;
   border: 6px solid #b33243;
-  border-radius: 100%;
+  border-radius: 50%;
 `;
 
 const AuthorDetails = styled.div`
   flex: 1;
   overflow-y: hidden;
   position: relative;
+  padding-right: 2rem;
   &::after {
     content: "";
     position: absolute;
-    background-image: linear-gradient(#ffffff00, #EFEFEF);
+    background-image: linear-gradient(#ffffff00, #efefef);
     bottom: 0;
     left: 0;
     right: 0;
@@ -93,23 +87,95 @@ const AuthorDetails = styled.div`
   } ;
 `;
 
+const ArticleNavigation = ({ prev, next }) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 1, textAlign: "left" }}>
+        {prev && (
+          <Link to={prev.frontmatter.path}>
+            PREVIOUS{" "}
+            <Visible md lg xl>
+              ARTICLE
+            </Visible>
+            <br />
+            <Meta>{prev.frontmatter.title}</Meta>
+          </Link>
+        )}
+      </div>
+      <div style={{ flex: 1, textAlign: "right" }}>
+        {next && (
+          <Link to={next.frontmatter.path}>
+            NEXT{" "}
+            <Visible md lg xl>
+              ARTICLE
+            </Visible>
+            <br />
+            <Meta>{next.frontmatter.title}</Meta>
+          </Link>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const AuthorCard = ({ author }) => {
+  const { isCompact } = useWindowWidth()
+  const [expanded, setExpanded] = useState()
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  };
+
+  return (
+    <FlexWrapper compact={isCompact}>
+      <PhotoWrapper>
+        <AuthorPhoto fixed={author.image.childImageSharp.fluid} />
+      </PhotoWrapper>
+
+      <div>
+        <AuthorDetails expand={expanded}>
+          {author.name && (
+            <h3>{author.name}</h3>
+          )}
+          {author.description && (
+            <Collapse 
+              in={expanded}  
+              collapsedSize="150px"
+              orientation="vertical"
+            >
+              <Markdown src={author.description} style={{marginTop: "0"}}/>
+            </Collapse>
+          )}
+        </AuthorDetails>
+        <Stack alignItems="flex-end">
+          <Button
+            onClick={handleExpandClick}
+            endIcon={<ExpandIcon expanded={expanded } />}
+            aria-expanded={expanded }
+            aria-label="read more"
+            size="small"
+            sx={{color: "var(--color-blueberry-dark)"}}
+          >
+            {expanded ? "Read Less" : "Read More"}
+          </Button>
+        </Stack>
+      </div>
+    </FlexWrapper>
+  )
+}
+
+const TagsList = ({ tags }) => (
+  <Meta>
+    <InlineList2
+      title="Tags"
+      items={tags.map(tag => <TagLink key={`tag-${ tag }`} tag={tag} />)}
+    />
+  </Meta>
+)
+
 export default ({ data, pageContext }) => {
   const { markdownRemark: { frontmatter, html, timeToRead } } = data;
   const { prev, next } = pageContext;
-  const { isCompact } = useWindowWidth();
-  const [expandedIndices, setExpandedIndices] = useState(new Set())
-
-  const handleExpandClick = index => {
-    // copy of expandedIndices state variable
-    const newExpandedIndices = new Set([...expandedIndices])
-    //remove if there ; add if not
-    if (newExpandedIndices.has(index)) {
-      newExpandedIndices.delete(index)
-    } else {
-      newExpandedIndices.add(index)
-    }
-    setExpandedIndices(newExpandedIndices)
-  };
 
   return (
     <PageContent width="95%" maxWidth="1200px" center gutters>
@@ -118,123 +184,51 @@ export default ({ data, pageContext }) => {
         description={frontmatter.seo.description}
         keywords={frontmatter.seo.keywords}
       />
-      <div className="news-item-container">
-        <div className="news-item">
-          <Title>{frontmatter.title}</Title>
-          <Subtitle className="article-subtitle">
-            {frontmatter.subtitle}
-          </Subtitle>
-          <div style={{display: "flex", justifyContent: "space-between", flexDirection: "row"}}>
-            <Meta>
-              Published on {frontmatter.date} {frontmatter.author && 
-              (<span>| Authored by {frontmatter.author}</span>)
-              }
-            </Meta>
-            <Meta>
-              {timeToRead} minute read
-            </Meta>
-          </div>
+      <Title>{frontmatter.title}</Title>
+      <Subtitle className="article-subtitle">
+        {frontmatter.subtitle}
+      </Subtitle>
+      <Stack justifyContent="space-between" flexDirection="row">
+        <Meta>
+          Published on {frontmatter.date} {' '}
+          {frontmatter.author && <span>| Authored by {frontmatter.author}</span>}
+        </Meta>
+        <Meta>
+          {timeToRead} minute read
+        </Meta>
+      </Stack>
 
-          <Meta>
-            <InlineList2
-              title="Tags"
-              items={frontmatter.tags.map((tag) => (
-                <TagLink tag={tag} />
-              ))}
-            />
-          </Meta>
-          {frontmatter.speakerImage && (
-            <SpeakerImageWrapper>
-              <SpeakerImage fluid={frontmatter.speakerImage.childImageSharp.fluid}/>
-            </SpeakerImageWrapper>
-          )}
-          <div className="page-content" dangerouslySetInnerHTML={{ __html: html }} />
+      <TagsList tags={frontmatter.tags} />
 
-          {/* author block */}
-          {frontmatter.authors && (
-            <Fragment>
-              <HorizontalRule />
-              
-              <Heading>About This Article</Heading>
-              <Paragraph>
-                This article was written with information provided by the following participants.
-              </Paragraph>
-            </Fragment>
-          )}
+      {frontmatter.speakerImage && (
+        <SpeakerImageWrapper>
+          <SpeakerImage fluid={frontmatter.speakerImage.childImageSharp.fluid}/>
+        </SpeakerImageWrapper>
+      )}
+      <div className="page-content" dangerouslySetInnerHTML={{ __html: html }} />
 
-          {frontmatter.authors && frontmatter.authors.map((author, id)=> {
-            const expanded = expandedIndices.has(id)
+      {/* author block */}
+      {frontmatter.authors && (
+        <Fragment>
+          <HorizontalRule />
+          
+          <Heading>About This Article</Heading>
+          <Paragraph>
+            This article was written with information provided by the following participants.
+          </Paragraph>
+        </Fragment>
+      )}
 
-            return (
-            <div key={`author-${id}`}>
-                <FlexWrapper compact={isCompact}>
-                  <PhotoWrapper>
-                    <AuthorPhoto fixed={author.image.childImageSharp.fluid} />
-                  </PhotoWrapper>
-
-                  <div>
-                    <AuthorDetails expand={expanded}>
-                      {author.name && (
-                        <h3>{author.name}</h3>
-                      )}
-                      {author.description && (
-                        <Collapse 
-                          in={expanded}  
-                          collapsedSize="150px"
-                          orientation="vertical"
-                        >
-                          <Markdown src={author.description} style={{marginTop: "0"}}/>
-                        </Collapse>
-                      )}
-                    </AuthorDetails>
-                    <div style={{display: "flex", justifyContent: "flex-end"}}>
-                      <ButtonBase onClick={() => handleExpandClick(id)} sx={{color: "#01366a"}}>
-                        {expanded ? "Read Less" : "Read More"}
-                        <ExpandMore
-                          expand={expanded}
-                          aria-expanded={expanded}
-                          aria-label="read more"
-                        >
-                          <ExpandMoreIcon />
-                        </ExpandMore>
-                      </ButtonBase>
-                    </div>
-                  </div>
-                </FlexWrapper>
-            </div>
-          )}
-          )}
+      {frontmatter.authors && frontmatter.authors.map((author, id) => (
+        <div key={`author-${id}`}>
+          <AuthorCard author={author} />
         </div>
-      </div>
+      ))}
 
       <HorizontalRule />
 
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 1, textAlign: "left" }}>
-          {prev && (
-            <Link to={prev.frontmatter.path}>
-              PREVIOUS{" "}
-              <Visible md lg xl>
-                ARTICLE
-              </Visible>
-              <br />
-              <Meta>{prev.frontmatter.title}</Meta>
-            </Link>
-          )}
-        </div>
-        <div style={{ flex: 1, textAlign: "right" }}>
-          {next && (
-            <Link to={next.frontmatter.path}>
-              NEXT{" "}
-              <Visible md lg xl>
-                ARTICLE
-              </Visible>
-              <br />
-              <Meta>{next.frontmatter.title}</Meta>
-            </Link>
-          )}
-        </div>
-      </div>
+      <ArticleNavigation prev={prev} next={next} />
+
     </PageContent>
   );
 };
