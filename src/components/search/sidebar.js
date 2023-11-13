f9e2d3b1719905ebb05d8724119cccb19dded64f
Matt Watson <mwatson@renci.org>
2023-11-13 11:34:22 -0500

    show/hide collection
    remove slide
    
    conditionally render

--- src/components/search/sidebar.js
+++ src/components/search/sidebar.js
@@ -1,7 +1,30 @@
-import React from "react";
-import { Stack } from "@mui/material";
+import React, { Fragment, createContext, useContext, useMemo } from 'react'
+import { Badge, Button, IconButton, Stack, Tooltip } from '@mui/material'
+import {
+  BookmarkBorder as CollectionIcon,
+} from '@mui/icons-material'
+import { useSearch } from './'
 
 //
+
+export const SidebarToggler = ({ visible = false, onClick, sx }) => {
+  const { collection } = useSearch()
+
+  return (
+    <Button
+      color="secondary"
+      size="large"
+      variant="text"
+      endIcon={
+        <Badge badgeContent={ collection.count } color="primary">
+          <CollectionIcon />
+        </Badge>
+      }
+      onClick={ onClick }
+      sx={ sx }
+    >{ visible ? 'Hide Collection' : 'View Collection' }</Button>
+  )
+}
 
 export const Sidebar = ({ children }) => {
   return (
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { Grid, Stack, Typography } from "@mui/material";
-import { PageContent } from "../../components/layout";
-import { SEO } from "../../components/seo";
+import React, { useMemo, useState } from 'react'
+import { Box, Grid, Stack, Typography } from '@mui/material'
+import { PageContent } from '../../components/layout'
+import { SEO } from '../../components/seo'
 import {
   BackToTopButton, CheckoutCta, CollectionPreview, DugForm,
-  FiltersTray, Results, Sidebar, useSearch,
-} from "../../components/search";
+  FiltersTray, Results, Sidebar, SidebarToggler, useSearch,
+} from '../../components/search'
 
 const SearchPage = ({ location }) => {
   const { query } = useSearch()
+  const [sidebarVisibility, setSidebarVisibility] = useState(true)
+
+  const mainContentSizes = useMemo(() => {
+    if (sidebarVisibility) {
+      return { xs: 12, md: 8, lg: 9 }
+    }
+    return { xs: 12, md: 12, lg: 12 }
+  }, [sidebarVisibility])
+
+  const sidebarSizes = useMemo(() => {
+    if (sidebarVisibility) {
+      return { xs: 12, md: 4, lg: 3 }
+    }
+    return { xs: 0, md: 0, lg: 0 }
+  }, [sidebarVisibility])
+
+  const handleClickToggleSidebar = () => setSidebarVisibility(!sidebarVisibility)
 
   return (
     <PageContent width="95%" maxWidth="1400px" center gutters>
@@ -17,11 +34,22 @@ const SearchPage = ({ location }) => {
         description="BioData Catalyst semantic search provided by Dug"
       />
 
-      <Typography variant="h1">Semantic Search</Typography>
+      <Box sx={{ position: 'relative' }}>
+        <Typography variant="h1">Semantic Search</Typography>
+        <SidebarToggler
+          visible={ sidebarVisibility }
+          onClick={ handleClickToggleSidebar }
+          sx={{
+            position: 'absolute',
+            right: '0px',
+            bottom: '8px',
+          }}
+        />
+      </Box>
 
       <Grid container spacing={ 4 }>
-        <Grid item xs={ 12 } md={ 8 } lg={ 9 }>
-          <Stack sx={{ position: 'relative' }}>
+        <Grid item { ...mainContentSizes }>
+          <Stack>
             <DugForm focusOnMount slashFocus />
             <FiltersTray />
             <Results />
@@ -29,14 +57,21 @@ const SearchPage = ({ location }) => {
           </Stack>
         </Grid>
 
-        <Grid item xs={ 12 } md={ 4 } lg={ 3 } sx={{
-          display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
-        }}>
-          <Sidebar>
-            <CollectionPreview />
-            <CheckoutCta />
-          </Sidebar>
-        </Grid>
+        {
+          sidebarVisibility && <Grid item
+            { ...sidebarSizes }
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Sidebar>
+              <CollectionPreview />
+              <CheckoutCta />
+            </Sidebar>
+          </Grid>
+        }
       </Grid>
 
     </PageContent>
