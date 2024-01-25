e516fd90cf7a20e8c7a3db35231594b7811eb32b
suejinkim20 <kim.suejin.20@gmail.com>
2024-01-25 01:28:46 -0500

    update markdown component

--- src/components/markdown/markdown.js
+++ src/components/markdown/markdown.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react'
 import { Link } from '../link'
 import ReactMarkdown from 'react-markdown'
-
+import { Paragraph } from '../typography/paragraph'
 export const Markdown = ({ src }) => {
   const componentMap = useMemo(() => ({
     a: function Anchor({ node, href, children, ...props }) { return <Link to={ href } { ...props }>{ children }</Link> },
-    p: function Anchor({ node, href, children, ...props }) { return <p style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</p> },
+    p: function Anchor({ node, href, children, ...props }) { return <Paragraph style={{lineHeight: '1.5', letterSpacing: '0.2px' }}  { ...props }>{children}</Paragraph> },
     li: function Anchor({ node, href, children, ...props }) { return <li style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</li> },
   }), [])
 
