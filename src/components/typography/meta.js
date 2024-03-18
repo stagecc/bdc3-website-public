f472f943f66dd6ad7b7c93b75bba192cfd9081ac
suejinkim20 <kim.suejin.20@gmail.com>
2024-03-18 16:50:45 -0400

    add noMargin prop to meta typography component

--- src/components/typography/meta.js
+++ src/components/typography/meta.js
@@ -4,6 +4,7 @@ import { Paragraph } from './paragraph'
 
 export const Meta = styled(Paragraph)`
     margin-bottom: 1rem;
+    ${props => props.noMargin && "margin-top: 0; margin-bottom: 0;"}
     font-weight: 300;
     font-size: 90%;
     color: var(--color-grey);
