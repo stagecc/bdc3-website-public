cc380c440c29a48b5a1261d2b7c331bfecb87c6e
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-18 15:35:53 -0400

    add content for about recover at bdc section

--- src/pages/recover.js
+++ src/pages/recover.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { SEO } from "../components/seo";
 import { PageContent } from "../components/layout";
-import { Title, Heading } from "../components/typography";
+import { Title, Heading, Paragraph, Subheading } from "../components/typography";
 import { Link } from "../components/link";
 import styled from "styled-components";
-import { List, ListItem } from "../components/list";
+import { List, BulletedList, OrderedList, ListItem } from "../components/list";
 import { Container as Grid, Row, Col, Visible } from "react-grid-system";
 import { navigate } from "gatsby";
 
@@ -70,6 +70,58 @@ const RECOVERPage = () => {
           <Col xs={12} lg={9}>
             <Section id="about-recover-at-bdc">
               <Heading>About RECOVER and its Research Community</Heading>
+              
+              <Paragraph>
+                <Link to="https://recovercovid.org/">
+                  RECOVER
+                </Link>{" "}is a first-of-its-kind, patient-centered research 
+                initiative to understand, diagnose, treat, and prevent Long 
+                COVID. RECOVER research includes observational cohort studies, 
+                electronic health records analysis, pathobiology studies, tissue 
+                pathology studies, and clinical trials. 
+              </Paragraph>
+
+              <Paragraph>
+                RECOVER studies involve thousands of people from all walks of 
+                life, hundreds of research investigators, and millions of 
+                electronic health records (EHRs). RECOVER aims to achieve the 
+                following:
+              </Paragraph>
+
+              <BulletedList>
+                <ListItem
+                  primary={
+                    <Fragment>
+                      Understand the range of recovery from Long COVID and the 
+                      changes it can cause in people over time.
+                    </Fragment>
+                  }
+                />
+                <ListItem
+                  primary={
+                    <Fragment>
+                      Define risk factors, understand the number of people 
+                      getting Long COVID, and determine whether there are 
+                      specific, different Long COVID types.
+                    </Fragment>
+                  }
+                />
+                <ListItem
+                  primary={
+                    <Fragment>
+                      Study how Long COVID changes over time and how those 
+                      changes may relate to other illnesses.
+                    </Fragment>
+                  }
+                />
+                <ListItem
+                  primary={
+                    <Fragment>
+                      Identify possible treatments for Long COVID symptoms.
+                    </Fragment>
+                  }
+                />
+              </BulletedList>
 
             </Section>
 
